94c485f863fd2d822cafcdb3362abedd5c1f45b0
Guilherme Cazaroto <gui.cazaroto@gmail.com>
2020-12-17 10:03:55 -0300

    [feature] create nuxt page/route

---
+++ pages/posts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <article>
+      <h1>Title of Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint eligendi
+        voluptatum iusto quaerat suscipit corporis distinctio omnis dicta, eius
+        quasi eaque? Deserunt veniam, corporis doloribus in suscipit fugiat
+        velit odit.
+      </p>
+    </article>
+  </div>
+</template>
